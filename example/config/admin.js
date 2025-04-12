3b90bf8cf576afbecf5921d5bbfb26c1f5a7a6f8
Václav Matouch <vaclav@matouch.cz>
2025-04-12 23:38:22 +0200

    chore: added config required by the current strapi version

--- example/config/admin.js
+++ example/config/admin.js
@@ -2,4 +2,10 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '4cd37781c90beeb5deb0bf90e1700221'),
   },
+  apiToken: { salt: env('API_TOKEN_SALT', 'test_api_token_salt_123') },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'test_transfer_token_salt_123'),
+    },
+  },
 });
